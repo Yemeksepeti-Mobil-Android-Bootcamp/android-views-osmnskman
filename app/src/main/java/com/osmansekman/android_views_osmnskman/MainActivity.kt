3261ed2098c00c36ba0c1e62613881509df9f0be
osmnskman <osmnskman@gmail.com>
2021-07-05 23:03:06 +0300

    first commit

---
+++ app/src/main/java/com/osmansekman/android_views_osmnskman/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.osmansekman.android_views_osmnskman
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        }
+
+    }
